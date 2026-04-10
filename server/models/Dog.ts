bc556cdae84a26b715503c7b3d2a48c27c874d42
stanton <stanton@projectinvicta.com>
2026-04-10 03:43:38 +0000

    remove typestripping from the config files

--- server/models/Dog.ts
+++
@@ -1,8 +0,0 @@
-import {Model, DataTypes} from "@projectinvicta/nails";
-
-export const schema = {
-  good: DataTypes.BOOLEAN,
-  name: DataTypes.STRING,
-};
-
-export default class Dog extends Model {};
